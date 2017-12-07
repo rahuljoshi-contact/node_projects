8682bab5b573bbd0a4e201db3e92ec4cab566268
rahuljoshi-contact <rahuljoshi.contact@gmail.com>
2017-12-07 16:22:00 -0500

    first node projects

---
+++ notes-node/app.js
@@ -0,0 +1,26 @@
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+
+
+var res = _.uniq([1, 2, 3, 2 ,2 ,1, 4, 'Andy', 'ANdy']);
+console.log(res);
+
+// console.log('12 is a number: '+ _.isNumber(12));
+// console.log('andry is a string: ' + _.isString('andy'));
+// var str = os.userInfo().username;
+// console.log(str);
+// // console.log(os.userInfo());
+//
+// const notes = require('./notes.js');
+// console.log(notes.add(10, 34));
+//
+// fs.appendFile('greetings.txt', '\r\nhello ' + str,
+// function (err) {
+//   if (err)
+//     console.log('some error occured');
+// }
+//
+//
+// );
---
+++ notes-node/notes.js
@@ -0,0 +1,9 @@
+//console.log(module);
+module.exports.addNote =  () => {
+  console.log('new note added');
+  return ('new note');
+}
+
+module.exports.add = (a,b) => {
+  return a+b;
+}
